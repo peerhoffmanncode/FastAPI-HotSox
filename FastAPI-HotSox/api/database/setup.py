6ce285f454e11c34b345c3b74d14c7373525ef2f
PeerHoffmannCode <peerhoffmann81@gmail.com>
2023-03-17 14:43:00 +0100

    added support for env file for database

--- FastAPI-HotSox/api/database/setup.py
+++ FastAPI-HotSox/api/database/setup.py
@@ -5,14 +5,14 @@ import os
 
 if os.path.isfile("env.py"):
     import env
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
+
+    DB_NAME = os.environ.get("DB_NAME")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASS = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST")
 
 SQLALCHAMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
-print(SQLALCHAMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
 
